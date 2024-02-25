a6bcab2b34d1389e3de93452ecf7a9a7850a3294
sureshgalla <sureshgalla149@gmail.com>
2024-02-25 21:12:37 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello {
+    public static void main(String a[]) {
+        int num = 7;
+        // int num2 = 5;
+        // int result = num1 % num2;
+        // num1 = num1 + 6;
+        // num1 += 2;
+        // num1++;
+        // num1--;
+        int result = ++num;
+        System.out.println(result);
+    }
+
+}
